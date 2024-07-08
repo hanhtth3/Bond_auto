dc86d8075a2bf804756f13bb7fa5364a1b3fa496
hanhtth3 <hanhtth3@gmail.com>
2024-07-08 10:23:09 +0700

    add bond auto
    add bond auto

--- src/test/java/com/vpbanks/features/managementcustomer/AddCustomerWhenValidData.java
+++ src/test/java/com/vpbanks/features/managementcustomer/AddCustomerWhenValidData.java
@@ -2,14 +2,14 @@ package com.vpbanks.features.managementcustomer;
 
 import com.vpbanks.tasks.Login;
 import com.vpbanks.ui.Elements;
+import io.cucumber.java.an.E;
 import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.SilentTask;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
-import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
-import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.actions.*;
+import org.htmlunit.javascript.host.event.MouseScrollEvent;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,7 +33,16 @@ public class AddCustomerWhenValidData {
     public void login_with_valid_user() {
 
         hanh.wasAbleTo(
-                Login.ValidUser("hanhtth@vpbanks.com.vn","Kofax@07")
+                Open.url(Elements.URL),
+               /* Enter.theValue("hanhtth@vpbanks.com.vn").into(Elements.USERNAME_FIELD),
+                Enter.theValue("Kofax@07").into(Elements.PASSWORD_FIELD),
+                Click.on(Elements.PASSWORD_FIELD),
+                Click.on(Elements.SUBMIT_BUTTON),
+                Click.on(Elements.SCROLL_MENU),
+                Click.on(Elements.IMG_BUTTON),
+                Click.on(Elements.BOND_MENU),
+                Click.on(Elements.CUSTOMER_MENU),*/
+                Click.on(Elements.ADDCUSTOMER_BUTTON)
 
         );
     }
--- src/test/java/com/vpbanks/ui/Elements.java
+++ src/test/java/com/vpbanks/ui/Elements.java
@@ -14,7 +14,7 @@ public class Elements {
             Target.the("password field")
                     .located(By.name("password"));
 
-    public static final String URL = "https://portal-sit.vpbanks.com.vn/login";
+    public static final String URL = "https://portal-sit.vpbanks.com.vn/bond/customer_info";
     public static final Target SUBMIT_BUTTON=
             Target.the("Đăng nhập button")
                     .locatedBy("//button");
@@ -27,4 +27,12 @@ public class Elements {
     public static final Target ADDCUSTOMER_BUTTON=
             Target.the("button Thêm mới khách hàng")
             .locatedBy("//span[text()='Thêm mới khách hàng']");
+
+    public static final Target SCROLL_MENU=
+            Target.the("menu Scroll")
+            .locatedBy("//div[@class='scrollbar-container main-menu-content ps']");
+
+    public static final Target IMG_BUTTON=
+            Target.the("img button")
+            .locatedBy("//a[@class='navbar-brand active']");
 }
